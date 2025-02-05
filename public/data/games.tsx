0bbf7c0b8691812752d7c44c389aaa579b5c795b
asoovision13 <asoovision13@gmail.com>
2025-02-05 16:50:50 +0100

    Add files via upload

---
+++ public/data/games.tsx
@@ -0,0 +1,478 @@
+export const games = [
+  {
+    name: "DLS 25",
+    logo: "dls25.jpg",
+    rating: 4,
+    shortcut: "DLS25",
+    coinlogo: "https://i.imgur.com/YvE1GPz.png",
+    backgroundImage: "https://st4.depositphotos.com/1001201/38408/i/450/depositphotos_384080930-stock-photo-football-stadium-with-the-stands.jpg",
+    coinOptions: [
+      { amount: '99k' },
+      { amount: '499k' },
+      { amount: '1M' },
+      { amount: '9M' }
+    ]
+  },
+  {
+    name: "Coin Master",
+    logo: "coinMaster.png",
+    rating: 5,
+    shortcut: "CM",
+    coinlogo: "https://i.imgur.com/YvE1GPz.png",
+    backgroundImage: "https://i.imgur.com/PGRRQLH.png",
+    coinOptions: [
+      { amount: '99M + 99K' },
+      { amount: '199M + 199K' },
+      { amount: '599M + 599K' },
+      { amount: '999M + 999K' }
+    ]
+  },
+  {
+    name: "FC Mobile",
+    logo: "fCMobile.png",
+    rating: 3,
+    shortcut: "FCM",
+    coinlogo: "https://i.imgur.com/YvE1GPz.png",
+    backgroundImage: "https://www.shutterstock.com/image-illustration/dynamic-stadium-large-modern-filled-600nw-2472452971.jpg",
+    coinOptions: [
+      { amount: '99K' },
+      { amount: '199K' },
+      { amount: '599K' },
+      { amount: '999K' }
+    ]
+  },
+  {
+    name: "eFootball™️",
+    logo: "eFootball.png",
+    rating: 3,
+    shortcut: "EF",
+    coinlogo: "https://i.imgur.com/g6kdAc7.png",
+    backgroundImage: "https://png.pngtree.com/background/20210706/original/pngtree-football-stadium-background-picture-image_245394.jpg",
+    coinOptions: [
+      { amount: '99K' },
+      { amount: '199K' },
+      { amount: '599K' },
+      { amount: '999K' }
+    ]
+  },
+  {
+    name: "Pocket FM",
+    logo: "pocketFM.png",
+    rating: 3,
+    shortcut: "PFM",
+    coinlogo: "https://i.imgur.com/PtThY0H.png",
+    backgroundImage: "https://is1-ssl.mzstatic.com/image/thumb/PurpleSource211/v4/3a/f0/64/3af06474-0935-6dc3-b5dc-288ebc6ccbed/15ebcd18-4662-46e3-a0d3-6ef7a4259171__U6b27_U7f8e-1242_2688-2.jpg/300x0w.jpg",
+    coinOptions: [
+      { amount: '99K' },
+      { amount: '199K' },
+      { amount: '599K' },
+      { amount: '1M' }
+    ]
+  },
+  {
+    name: "Cooking Fever",
+    logo: "cookingFever.png",
+    rating: 3,
+    shortcut: "CF",
+    coinlogo: "https://i.imgur.com/mki2SIB.png",
+    backgroundImage: "https://images.launchbox-app.com/7c5d3381-15cb-483c-883f-302ea083fbc5.jpg",
+    coinOptions: [
+      { amount: '99K' },
+      { amount: '199K' },
+      { amount: '599K' },
+      { amount: '999K' }
+    ]
+  },
+  {
+    name: "Dragon Mania Legends",
+    logo: "dragonManiaLegends.png",
+    rating: 3,
+    shortcut: "DML",
+    coinlogo: "https://i.imgur.com/8GczKD4.png",
+    backgroundImage: "https://i.imgur.com/fmNVPvn.png",
+    coinOptions: [
+      { amount: '99K' },
+      { amount: '199K' },
+      { amount: '599K' },
+      { amount: '999K' }
+    ]
+  },
+  {
+    name: "Dragon City",
+    logo: "dragonCity.png",
+    rating: 3,
+    shortcut: "DC",
+    coinlogo: "https://i.imgur.com/xfdRxIA.png",
+    backgroundImage: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/65db5761645499.5a74d1b1d6679.jpg",
+    coinOptions: [
+      { amount: '99K' },
+      { amount: '199K' },
+      { amount: '599K' },
+      { amount: '999K' }
+    ]
+  },
+  {
+    name: "Monopoly GO",
+    logo: "monopolyGO.png",
+    rating: 3,
+    shortcut: "MG",
+    coinlogo: "https://i.imgur.com/q67UEsk.png",
+    backgroundImage: "https://i.imgur.com/Tqh8M8i.png",
+    coinOptions: [
+      { amount: '99K' },
+      { amount: '199K' },
+      { amount: '599K' },
+      { amount: '999K' }
+    ]
+  },
+  {
+    name: "ReelShort",
+    logo: "reelShort.png",
+    rating: 3,
+    shortcut: "RS",
+    coinlogo: "https://i.imgur.com/nFfK2o3.png",
+    backgroundImage: "https://is1-ssl.mzstatic.com/image/thumb/PurpleSource211/v4/3a/f0/64/3af06474-0935-6dc3-b5dc-288ebc6ccbed/15ebcd18-4662-46e3-a0d3-6ef7a4259171__U6b27_U7f8e-1242_2688-2.jpg/300x0w.jpg",
+    coinOptions: [
+      { amount: '99K' },
+      { amount: '199K' },
+      { amount: '599K' },
+      { amount: '999K' }
+    ]
+  },
+  {
+    name: "Pokémon GO",
+    logo: "pokmonGO.png",
+    rating: 3,
+    shortcut: "PGO",
+    coinlogo: "/pokemongo-coin.png",
+    backgroundImage: "https://i.imgur.com/WNvKnwE.png",
+    coinOptions: [
+      { amount: '99M + 99K' },
+      { amount: '199M + 199K' },
+      { amount: '599M + 599K' },
+      { amount: '999M + 999K' }
+    ]
+  },
+  {
+    name: "Shadow Fight 3",
+    logo: "shadowFight3.png",
+    rating: 3,
+    shortcut: "SF3",
+    coinlogo: "https://i.imgur.com/TDfCKVJ.png",
+    backgroundImage: "https://i.imgur.com/bzq6Dwd.jpeg",
+    coinOptions: [
+      { amount: '99K' },
+      { amount: '199K' },
+      { amount: '599K' },
+      { amount: '999K' }
+    ]
+  },
+  {
+    name: "DramaBox",
+    logo: "dramaBox.png",
+    rating: 3,
+    shortcut: "DB",
+    coinlogo: "https://i.imgur.com/PtThY0H.png",
+    backgroundImage: "https://is1-ssl.mzstatic.com/image/thumb/PurpleSource211/v4/3a/f0/64/3af06474-0935-6dc3-b5dc-288ebc6ccbed/15ebcd18-4662-46e3-a0d3-6ef7a4259171__U6b27_U7f8e-1242_2688-2.jpg/300x0w.jpg",
+    coinOptions: [
+      { amount: '99K' },
+      { amount: '199K' },
+      { amount: '599K' },
+      { amount: '1M' }
+    ]
+  },
+  {
+    name: "Candy Crush Saga",
+    logo: "candyCrushSaga.png",
+    rating: 3,
+    shortcut: "CCS",
+    coinlogo: "https://static.wikia.nocookie.net/candy-crush-saga/images/6/67/Goldbar.png",
+    backgroundImage: "https://images-rsg.storage.googleapis.com/wp-content/uploads/2022/03/Bonbons-2D-Background-Levels-Games-Match-3-Candy-Crush.jpg",
+    coinOptions: [
+      { amount: '99K' },
+      { amount: '199K' },
+      { amount: '599K' },
+      { amount: '999K' }
+    ]
+  },
+  {
+    name: "Roblox",
+    logo: "roblox.png",
+    rating: 3,
+    shortcut: "RBX",
+    coinlogo: "/roblox-coin.png",
+    backgroundImage: "",
+    coinOptions: [
+      { amount: '99M + 99K' },
+      { amount: '199M + 199K' },
+      { amount: '599M + 599K' },
+      { amount: '999M + 999K' }
+    ]
+  },
+  {
+    name: "TikTok",
+    logo: "tikTok.png",
+    rating: 3,
+    shortcut: "TT",
+    coinlogo: "/tiktok-coin.png",
+    backgroundImage: "",
+    coinOptions: [
+      { amount: '99M + 99K' },
+      { amount: '199M + 199K' },
+      { amount: '599M + 599K' },
+      { amount: '999M + 999K' }
+    ]
+  },
+  {
+    name: "Top Eleven",
+    logo: "topEleven.png",
+    rating: 3,
+    shortcut: "TE",
+    coinlogo: "https://i.imgur.com/bJKDqUR.png",
+    backgroundImage: "https://i.imgur.com/TLZHEVk.png",
+    coinOptions: [
+      { amount: '99M + 99K' },
+      { amount: '199M + 199K' },
+      { amount: '599M + 599K' },
+      { amount: '999M + 999K' }
+    ]
+  },
+  {
+    name: "Call of Duty: Mobile",
+    logo: "callofDutyMobile.png",
+    rating: 3,
+    shortcut: "CODM",
+    coinlogo: "/codm-coin.png",
+    backgroundImage: "",
+    coinOptions: [
+      { amount: '99M + 99K' },
+      { amount: '199M + 199K' },
+      { amount: '599M + 599K' },
+      { amount: '999M + 999K' }
+    ]
+  },
+  {
+    name: "Baseball 9",
+    logo: "baseball9.png",
+    rating: 3,
+    shortcut: "BB9",
+    coinlogo: "/baseball9-coin.png",
+    backgroundImage: "",
+    coinOptions: [
+      { amount: '99M + 99K' },
+      { amount: '199M + 199K' },
+      { amount: '599M + 599K' },
+      { amount: '999M + 999K' }
+    ]
+  },
+  {
+    name: "Block Blast",
+    logo: "blockBlast.png",
+    rating: 3,
+    shortcut: "BB",
+    coinlogo: "/blockblast-coin.png",
+    backgroundImage: "",
+    coinOptions: [
+      { amount: '99M + 99K' },
+      { amount: '199M + 199K' },
+      { amount: '599M + 599K' },
+      { amount: '999M + 999K' }
+    ]
+  },
+  {
+    name: "Car Parking Multiplayer",
+    logo: "carParkingMultiplayer.png",
+    rating: 3,
+    shortcut: "CPM",
+    coinlogo: "/carparking-coin.png",
+    backgroundImage: "",
+    coinOptions: [
+      { amount: '99M + 99K' },
+      { amount: '199M + 199K' },
+      { amount: '599M + 599K' },
+      { amount: '999M + 999K' }
+    ]
+  },
+  {
+    name: "Subway Surfers",
+    logo: "subwaySurfers.png",
+    rating: 3,
+    shortcut: "SS",
+    coinlogo: "https://i.imgur.com/bJKDqUR.png",
+    backgroundImage: "https://media.sketchfab.com/models/baed87144cab479d91f851eadf678c96/thumbnails/7905fb7ef3e94103b0c92b2c45c4a87c/989a3e468c18406589f5181210940e39.jpeg",
+    coinOptions: [
+      { amount: '99K' },
+      { amount: '199K' },
+      { amount: '599K' },
+      { amount: '999K' }
+    ]
+  },
+  {
+    name: "Homescapes",
+    logo: "homescapes.png",
+    rating: 3,
+    shortcut: "HS",
+    coinlogo: "/homescapes-coin.png",
+    backgroundImage: "",
+    coinOptions: [
+      { amount: '99M + 99K' },
+      { amount: '199M + 199K' },
+      { amount: '599M + 599K' },
+      { amount: '999M + 999K' }
+    ]
+  },
+  {
+    name: "Gardenscapes",
+    logo: "gardenscapes.png",
+    rating: 3,
+    shortcut: "GS",
+    coinlogo: "/gardenscapes-coin.png",
+    backgroundImage: "",
+    coinOptions: [
+      { amount: '99M + 99K' },
+      { amount: '199M + 199K' },
+      { amount: '599M + 599K' },
+      { amount: '999M + 999K' }
+    ]
+  },
+  {
+    name: "Township",
+    logo: "township.png",
+    rating: 3,
+    shortcut: "TS",
+    coinlogo: "/township-coin.png",
+    backgroundImage: "",
+    coinOptions: [
+      { amount: '99M + 99K' },
+      { amount: '199M + 199K' },
+      { amount: '599M + 599K' },
+      { amount: '999M + 999K' }
+    ]
+  },
+  {
+    name: "Royal Match",
+    logo: "royalMatch.png",
+    rating: 3,
+    shortcut: "RM",
+    coinlogo: "/royalmatch-coin.png",
+    backgroundImage: "",
+    coinOptions: [
+      { amount: '99M + 99K' },
+      { amount: '199M + 199K' },
+      { amount: '599M + 599K' },
+      { amount: '999M + 999K' }
+    ]
+  },
+  {
+    name: "SimCity BuildIt",
+    logo: "simCityBuildIt.png",
+    rating: 3,
+    shortcut: "SCB",
+    coinlogo: "/simcity-coin.png",
+    backgroundImage: "",
+    coinOptions: [
+      { amount: '99M + 99K' },
+      { amount: '199M + 199K' },
+      { amount: '599M + 599K' },
+      { amount: '999M + 999K' }
+    ]
+  },
+  {
+    name: "Whiteout Survival",
+    logo: "whiteoutSurvival.png",
+    rating: 3,
+    shortcut: "WS",
+    coinlogo: "/whiteout-coin.png",
+    backgroundImage: "",
+    coinOptions: [
+      { amount: '99M + 99K' },
+      { amount: '199M + 199K' },
+      { amount: '599M + 599K' },
+      { amount: '999M + 999K' }
+    ]
+  },
+  {
+    name: "RealRacing3",
+    logo: "realRacing3.png",
+    rating: 3,
+    shortcut: "RR3",
+    coinlogo: "/realracing3-coin.png",
+    backgroundImage: "",
+    coinOptions: [
+      { amount: '99M + 99K' },
+      { amount: '199M + 199K' },
+      { amount: '599M + 599K' },
+      { amount: '999M + 999K' }
+    ]
+  },
+  {
+    name: "The Sims™ FreePlay",
+    logo: "theSimsFreePlay.png",
+    rating: 3,
+    shortcut: "TSFP",
+    coinlogo: "/thesimsfreeplay-coin.png",
+    backgroundImage: "",
+    coinOptions: [
+      { amount: '99M + 99K' },
+      { amount: '199M + 199K' },
+      { amount: '599M + 599K' },
+      { amount: '999M + 999K' }
+    ]
+  },
+  {
+    name: "Pokémon TCG Pocket",
+    logo: "pokmonTCGPocket.png",
+    rating: 3,
+    shortcut: "PTCGP",
+    coinlogo: "/pokemontcg-coin.png",
+    backgroundImage: "",
+    coinOptions: [
+      { amount: '99M + 99K' },
+      { amount: '199M + 199K' },
+      { amount: '599M + 599K' },
+      { amount: '999M + 999K' }
+    ]
+  },
+  {
+    name: "Bitlife",
+    logo: "bitlife.png",
+    rating: 3,
+    shortcut: "BL",
+    coinlogo: "/bitlife-coin.png",
+    backgroundImage: "",
+    coinOptions: [
+      { amount: '99M + 99K' },
+      { amount: '199M + 199K' },
+      { amount: '599M + 599K' },
+      { amount: '999M + 999K' }
+    ]
+  },
+  {
+    name: "LastWar",
+    logo: "lastWar.png",
+    rating: 3,
+    shortcut: "LW",
+    coinlogo: "/lastwar-coin.png",
+    backgroundImage: "",
+    coinOptions: [
+      { amount: '99M + 99K' },
+      { amount: '199M + 199K' },
+      { amount: '599M + 599K' },
+      { amount: '999M + 999K' }
+    ]
+  },
+  {
+    name: "Archero 2",
+    logo: "achero2.png",
+    rating: 3,
+    shortcut: "A2",
+    coinlogo: "/archero2-coin.png",
+    backgroundImage: "",
+    coinOptions: [
+      { amount: '99M + 99K' },
+      { amount: '199M + 199K' },
+      { amount: '599M + 599K' },
+      { amount: '999M + 999K' }
+    ]
+  }
+];
